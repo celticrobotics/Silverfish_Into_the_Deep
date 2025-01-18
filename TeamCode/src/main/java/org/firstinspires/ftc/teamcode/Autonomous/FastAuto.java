2fb26afd0dd793070a5c10566e0a19639239b183
Emmy Hylton <emmanuellehylton25@stpats.bc.ca>
2025-01-18 06:09:20 -0800

    new auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/FastAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/FastAuto.java
@@ -17,25 +17,26 @@ public class FastAuto extends LinearOpMode {
         Auto.Initialization();
 
         // Pre-Load Sample
+        Auto.moveVerticalLinearSlide(1, 300);
         Auto.strafe(0.5, -10, 0.8);
-        Auto.fwd(0.5, -10, 0.8);
-        Auto.turn(0.5, -55, 0.7);
-        Auto.fwd(0.5, -5, 1);
+        Auto.fwd(0.5, -8, 0.8);
+        Auto.turn(0.5, -62.069, 0.7);
+        Auto.fwd(0.5, -7, 1);
         Auto.moveVerticalLinearSlide(1, 4000);
         Auto.rotateThingyIntoBasket(2);
         Auto.origin(2);
 
         // 1st Sample
-        Auto.fwd(0.5, 12, 1);
-        Auto.turn(0.5, -45, 1);
-        Auto.fwd(0.5, 7, 1);
+        Auto.fwd(0.5, 10.5, 1);
+        Auto.turn(0.5, -42.069, 1);
+        Auto.fwd(0.5, 10.2, 1);
         Auto.ClawMove(AutonomousTestV2withsigmaBrandonandOliver.ClawPosition.CLOSED, 0.7);
         Auto.ElbowMove(AutonomousTestV2withsigmaBrandonandOliver.ElbowPos.UP, 0.7);
         Auto.ClawMove(AutonomousTestV2withsigmaBrandonandOliver.ClawPosition.OPEN, 0.7);
         Auto.ElbowMove(AutonomousTestV2withsigmaBrandonandOliver.ElbowPos.DOWN, 0.7);
         Auto.fwd(0.5, -10, 1);
         Auto.turn(0.5, 45, 1);
-        Auto.fwd(0.5, -7, 1);
+        Auto.fwd(0.5, -9, 1);
         Auto.moveVerticalLinearSlide(1, 4000);
         Auto.rotateThingyIntoBasket(2);
         Auto.origin(2);
